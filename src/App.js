9970cb3f82c4e5d6cfa11fee4763a24144aeac1d
abdulbutt403 <75443479+abdulbutt403@users.noreply.github.com>
2023-10-16 18:47:03 +0200

    Private routes

--- src/App.js
+++ src/App.js
@@ -28,53 +28,55 @@ import TrustCenter from "./pages/TrustCenter";
 import Users from "./pages/Users";
 import { Toaster } from "react-hot-toast";
 import Reset from "./pages/Reset";
+import PrivateRoutes from "./PrivateRoute";
 
 function App() {
   return (
     <>
-    <Toaster toastOptions={{
-    className: '',
-    style: {
-      border: '1px solid #713200',
-      padding: '16px',
-      color: '#713200',
-      zIndex: 99999999
-    },
-  }}/>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/reset" element={<Reset />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/blogPost/:id" element={<BlogPost />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/admin" element={<Navigate to="/admin/links" />} />
-        <Route path="/admin/links" element={<Admin />} />
-        <Route path="/admin/appearance" element={<Appearance />} />
-        <Route path="/admin/account" element={<Account />} />
-        <Route path="/admin/settings" element={<Settings />} />
-        <Route path="/admin/analytics" element={<Analytics />} />
-        <Route path="/help" element={<Help />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/marketplace" element={<MarketPlace />} />
-        <Route path="/browse" element={<Browse />} />
-        <Route path="/cookie" element={<Cookie />} />
-        <Route path="/trust-center" element={<TrustCenter />} />
-        <Route path="/report-problem" element={<Report />} />
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            border: "1px solid #713200",
+            padding: "16px",
+            color: "#713200",
+            zIndex: 99999999,
+          },
+        }}
+      />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogPost/:id" element={<BlogPost />} />
+          <Route path="/contact" element={<Contact />} />
 
+          <Route element={<PrivateRoutes/>}>
+            <Route path="/admin" element={<Navigate to="/admin/links" />} />
+            <Route path="/admin/links" element={<Admin />} />
+            <Route path="/admin/appearance" element={<Appearance />} />
+            <Route path="/admin/account" element={<Account />} />
+            <Route path="/admin/settings" element={<Settings />} />
+            <Route path="/admin/analytics" element={<Analytics />} />
+          </Route>
 
-        
-      </Routes>
-    </Router>
+          <Route path="/help" element={<Help />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/marketplace" element={<MarketPlace />} />
+          <Route path="/browse" element={<Browse />} />
+          <Route path="/cookie" element={<Cookie />} />
+          <Route path="/trust-center" element={<TrustCenter />} />
+          <Route path="/report-problem" element={<Report />} />
+        </Routes>
+      </Router>
     </>
   );
 }
 
 export default App;
-
-
