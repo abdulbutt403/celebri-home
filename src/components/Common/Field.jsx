a04faf0288b9e36c7d1503efdf4ee7acf77e0c06
abdulbutt403 <75443479+abdulbutt403@users.noreply.github.com>
2023-12-05 19:16:24 +0100

    show hide password

--- src/components/Common/Field.jsx
+++ src/components/Common/Field.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { EyeOpen } from "../../svgs";
+import { EyeClose, EyeOpen } from "../../svgs";
+import { useState } from "react";
 
 export default function Field({ type, placeholder, stickyLabel, value, onChange }) {
+  const [show,setShow] = useState(false)
   return (
     <div className="field">
       {stickyLabel && <label>linktr.ee/</label>}
       <div style={{ flexGrow: "1"}}>
-        <input type={type} placeholder={placeholder} style={{width: '100%'}} value={value} onChange={onChange}/>
-        {type=== 'password' && <span style={{position: 'absolute', right: 15, top: 17, cursor: 'pointer'}}><EyeOpen/></span>}
+        <input type={show ? 'text' : type} placeholder={placeholder} style={{width: '100%'}} value={value} onChange={onChange}/>
+        {type=== 'password' && <span style={{position: 'absolute', right: 15, top: 17, cursor: 'pointer'}} onClick={() => setShow(x => !x)}>{!show ? <EyeClose/>: <EyeOpen/>}</span>}
       </div>
     </div>
   );
